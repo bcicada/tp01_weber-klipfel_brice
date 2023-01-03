b1dce02d157f65f19df1c29aaabff6d4f80f9fac
bcicada <b.cicada@protonmail.com>
2023-01-03 14:50:13 +0000

    Init

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'tp_01_weber-klipfel_brice';
+}
